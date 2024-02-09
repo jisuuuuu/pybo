17e28ace998b25a1d6b8db759feb05774183fde3
jisu <ihjs901@naver.com>
2024-02-09 16:57:32 +0900

    일부러 오류 발생

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from ..models import Question
 
 def index(request):
+    3/0 #강제로 오류발생
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
